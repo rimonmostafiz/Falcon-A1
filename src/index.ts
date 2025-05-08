7be3b17cdcd7522a05dd7e3c3a66c1b64e749d65
Rimon Mostafiz <rimonmostafiz@gmail.com>
2025-05-08 03:18:20 -0500

    Aura Vortex, Blazy - Horizons in sight :rocket:

--- src/index.ts
+++ src/index.ts
@@ -58,3 +58,16 @@ function getMostExpensiveProduct(products: Product[]): Product | null {
     }
 }
 
+enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+function getDayType(day: Day): string {
+    return day === Day.Saturday || day === Day.Sunday ? "Weekend" : "Weekday";
+}
