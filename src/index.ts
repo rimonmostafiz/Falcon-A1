b5b5a34fd5356e87e75a7620b466047c7e4a2568
Rimon Mostafiz <rimonmostafiz@gmail.com>
2025-05-08 02:55:38 -0500

    Falcon-A1 trajectory on point :dizzy:

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,30 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
     arrays.forEach(item => result.push(...item));
     return result;
 }
+
+class Vehicle {
+    private make: string;
+    public year: number;
+
+    constructor(make: string, year: number) {
+        this.make = make;
+        this.year = year;
+    }
+
+    public getInfo(): string {
+        return `"Make: ${this.make}, Year: ${this.year}"`;
+    }
+}
+
+class Car extends Vehicle {
+    private model: string;
+
+    constructor(make: string, year: number, model: string) {
+        super(make, year);
+        this.model = model;
+    }
+
+    public getModel(): string {
+        return `"Model: ${this.model}"`;
+    }
+}
